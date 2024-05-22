12d3fd26c28aa4ebb040cb8576ca86b7b0d52af1
r.alonso <r.alonso@opendeusto.es>
2024-05-22 16:53:08 +0200

    clase admin

--- Servidor_CPP_Biblioteca/Admin.cpp
+++ Servidor_CPP_Biblioteca/Admin.cpp
@@ -1,15 +1,32 @@
-/*
- * Admin.cpp
- *
- *  Created on: 22 may 2024
- *      Author: ruby2
- */
-
 #include "Admin.h"
 
 Admin::Admin() {
-	// TODO Auto-generated constructor stub
 
+}
+
+Admin::Admin(const std::string& n,const std::string& a, const std::string& d, const std::string& nt, const std::string& c)
+: nombre(n), apellido(a), dni(d), numTarjeta(nt), contrasenya(c) {
+
+}
+
+const std::string& Admin::getNombre() const{
+	return nombre;
+}
+
+const std::string& Admin::getApellido() const{
+	return apellido;
+}
+
+const std::string& Admin::getDni() const{
+	return dni;
+}
+
+const std::string& Admin::getNumTarjeta() const{
+	return numTarjeta;
+}
+
+const std::string& Admin::getContrasenya() const{
+	return contrasenya;
 }
 
 Admin::~Admin() {
--- Servidor_CPP_Biblioteca/Admin.h
+++ Servidor_CPP_Biblioteca/Admin.h
@@ -1,16 +1,20 @@
-/*
- * Admin.h
- *
- *  Created on: 22 may 2024
- *      Author: ruby2
- */
-
 #ifndef ADMIN_H_
 #define ADMIN_H_
-
+#include <string>
 class Admin {
+	std:: string nombre;
+	std:: string apellido;
+	std:: string dni;
+	std:: string numTarjeta;
+	std:: string contrasenya;
 public:
 	Admin();
+	Admin(const std::string& n, const std::string& a, const std::string& d, const std::string& nt, const std::string& c);
+	const std:: string& getNombre()const;
+	const std:: string& getApellido()const;
+	const std:: string& getDni()const;
+	const std:: string& getNumTarjeta()const;
+	const std:: string& getContrasenya()const;
 	virtual ~Admin();
 };
 
