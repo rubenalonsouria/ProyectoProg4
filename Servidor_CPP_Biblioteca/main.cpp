aa96ce069b619c0bdc6ba368ff4d679ccbb85ec9
Enrique <enrique.corres@opendeusto.es>
2024-05-22 17:02:23 +0200

    Creacion del fichero de configuración y lectura del mismo en Main

--- Servidor_CPP_Biblioteca/main.cpp
+++ Servidor_CPP_Biblioteca/main.cpp
@@ -5,10 +5,27 @@
 #include "BaseDeDatos.h"
 #include "Usuario.h"
 #include "Libro.h"
-
+#include <fstream>
+#include <iostream>
+using namespace std;
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 6000
+#define NOMFICH "Configuracion.txt"
+void leerFicheroConfig(char *nomfich, char *nombre_bd, char *server_ip,
+		int *server_port) {
+	ifstream ifs(nomfich);
+	char linea[200];
+	ifs >> linea;
+	strtok(linea, "=");
+	sprintf(nombre_bd, strtok(NULL, ""));
+	ifs >> linea;
+	strtok(linea, "=");
+	sprintf(server_ip, strtok(NULL, ""));
+	ifs >> linea;
+	strtok(linea, "=");
+	*server_port = atoi(strtok(NULL, ""));
 
+}
 int main(int argc, char *argv[]) {
 
 	WSADATA wsaData;
@@ -17,7 +34,12 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in server;
 	struct sockaddr_in client;
 	char sendBuff[512], recvBuff[512];
+	char server_ip[16],nombre_bd[30];
+	int server_port;
 
+	leerFicheroConfig(NOMFICH,nombre_bd,server_ip,&server_port);
+
+	cout<<nombre_bd<<" "<<server_ip<<" "<<server_port<<endl;
 	printf("\nInitialising Winsock...\n");
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		printf("Failed. Error Code : %d", WSAGetLastError());
@@ -35,9 +57,11 @@ int main(int argc, char *argv[]) {
 
 	printf("Socket created.\n");
 
-	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	//server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server.sin_addr.s_addr = inet_addr(server_ip);
 	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT);
+	//server.sin_port = htons(SERVER_PORT);
+	server.sin_port = htons(server_port);
 
 	//BIND (the IP/port with socket)
 	if (bind(conn_socket, (struct sockaddr*) &server,
@@ -77,9 +101,11 @@ int main(int argc, char *argv[]) {
 
 	//SEND and RECEIVE data
 	printf("Waiting for incoming messages from client... \n");
-	BaseDeDatos bd("LibreriaDeusto.db");
+	//BaseDeDatos bd("LibreriaDeusto.db");
+	BaseDeDatos bd(nombre_bd);
 	bd.conectar();
-	char opcion, opcionU, nombre[20], contrasenya[20],dni[8],numTarjeta[16],apellido[20];
+	char opcion, opcionU, nombre[20], contrasenya[20], dni[8], numTarjeta[16],
+			apellido[20];
 	char titulo[30];
 	char editorial[20];
 	char autor[20];
@@ -118,28 +144,30 @@ int main(int argc, char *argv[]) {
 							recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
 							sprintf(isbn, "%s", recvBuff);
 							Libro l(titulo, editorial, autor, isbn);
-							aniadirLibroaFichero(l);         //Mejor añadir a la bbdd
+							aniadirLibroaFichero(l);    //Mejor añadir a la bbdd
 							break;
 						case '2':
 							recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
 							sprintf(titulo, "%s", recvBuff);
 							pos = buscarLibro(lL, titulo);
 							if (pos == -1) {
-								sprintf(sendBuff,"No existe ese libro\n");
-								send(comm_socket, sendBuff, sizeof(sendBuff), 0);
+								sprintf(sendBuff, "No existe ese libro\n");
+								send(comm_socket, sendBuff, sizeof(sendBuff),
+										0);
 
 							} else {
 								eliminarLibro(&lL, pos);
-								sprintf(sendBuff,"Libro devuelto\n");
-								send(comm_socket, sendBuff, sizeof(sendBuff), 0);
+								sprintf(sendBuff, "Libro devuelto\n");
+								send(comm_socket, sendBuff, sizeof(sendBuff),
+										0);
 
 							}
 							break;
 						case '3':
 							break;
 						case '4':
-							sprintf(sendBuff,"%s",titulo);
-							send(s,sendBuff,sizeof(sendBuff),0);
+							sprintf(sendBuff, "%s", titulo);
+							send(s, sendBuff, sizeof(sendBuff), 0);
 							pos = buscarLibro(lL, titulo);
 
 							break;
@@ -166,15 +194,16 @@ int main(int argc, char *argv[]) {
 
 			recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
 			sprintf(contrasenya, "%s", recvBuff);
-			Usuario usu(dni,nombre,apellido,numTarjeta,contrasenya);
+			Usuario usu(dni, nombre, apellido, numTarjeta, contrasenya);
 
 			//pos = buscarUsuario(lu, nombre);
 			pos = bd.buscarUsuario(nombre);
-			if(pos!=-1){
-				sprintf(sendBuff,"Ese nombre de usuario ya existe\n");
+			if (pos != -1) {
+				sprintf(sendBuff, "Ese nombre de usuario ya existe\n");
 				send(comm_socket, sendBuff, sizeof(sendBuff), 0);
-			}else{
-				sprintf(sendBuff,"Se ha registrado correctamente el usuario \n");
+			} else {
+				sprintf(sendBuff,
+						"Se ha registrado correctamente el usuario \n");
 				send(comm_socket, sendBuff, sizeof(sendBuff), 0);
 				aniadirUsuario(&lu, usu);
 				bd.insertarUsuario(usu);
